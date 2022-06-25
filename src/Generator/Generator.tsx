ad8b9decf0563c19ef35b554a32af2d570de90b1
Adam Y. Cole II <afewburritos@gmail.com>
2022-06-25 16:23:45 -0500

    feat: it's-a-MUI
    not sorry

--- src/Generator/Generator.tsx
+++ src/Generator/Generator.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import InputHell from "./InputHell/InputHell";
 
 type Props = {};
 
 const Generator = (_props: Props) => {
-  return <div>Generator</div>;
+  return (
+    <div>
+      <InputHell />
+    </div>
+  );
 };
 
 export default Generator;
---
+++ src/Generator/InputHell/InputHell.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TextField from "@mui/material/textfield";
+
+type Props = {};
+
+const InputHell = (props: Props) => {
+  return <TextField label="Placeholder" />;
+};
+
+export default InputHell;
